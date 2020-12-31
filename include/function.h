37ad028201c1135c0c84f6ca014145aa2c43f230
Céline Ly <73025983+LyCeline06@users.noreply.github.com>
2020-12-31 11:50:57 +0100

    +commentaire

--- include/function.h
+++ include/function.h
@@ -6,21 +6,50 @@
 
 #include "token_tab.h"
 
-typedef struct typelist
+typedef struct typelist //structure for a lsit of arguments
 {
-	int type;
-	char *name;
-	struct typelist* next;
+	int type; 					//type of argument
+	char *name;					//name of argument when declared
+	struct typelist* next;		//next argument
 } typelist ;
 
-
+/*
+* initialize a typelist with a name and a type
+*/
 typelist* create_typelist(char *id, char *type);
+
+/*
+* concat 2 arglist
+*/
 typelist* add_to_typelist(typelist *l1, typelist *l2);
+
+/*
+* compare 2 arglist, returns an error when different
+*/
 void cmp_typelist(typelist *l1, typelist *l2);
+
 void print_par(typelist *l);
+
+/*
+* returns a typelist for a function id
+*/
 typelist* get_typelist(char *id);
+
+/*
+* checks if a function id exists in the symb_tab
+* returns an error when not found
+*/
 void chk_symb_fct(char *id);
+
+/*
+* add the typelist to the function id P_symb in the symb_tab
+*/
 void add_typelist_to_symb(char *id, typelist* arg);
+
+/*
+* does a gencode for the parameters of function
+* returns the number of param
+*/
 int gencode_param(typelist* arg);
 
 
