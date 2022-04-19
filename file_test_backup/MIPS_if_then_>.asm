2f84e48d02cdd9fcc02308d3c8e59c67ec419af4
Mis0ko <misoko.hofmann@gmail..com>
2022-04-20 00:15:58 +0200

    final commit

---
+++ file_test_backup/MIPS_if_then_>.asm
@@ -0,0 +1,43 @@
+#	SCALPA Project
+#	Output test code in MIPS
+#	
+
+
+
+.data
+# In the section .data we establish data components such as 
+# variables or strings to be displayed on the console. 
+
+
+
+.text
+# In the section .text we put our executable code 
+
+LABEL_0:
+		li $t0, 1
+		sw $t0, VAR_i_
+
+LABEL_1:
+		li $t0, 2
+		sw $t0, VAR_j_
+
+LABEL_2:
+		lw $t0, VAR_i_
+		lw $t1, VAR_j_
+		ble $t1, $t2, LABEL_4
+
+LABEL_3:
+		j LABEL_5
+
+LABEL_4:
+		lw $t0, VAR_j_
+		sw $t0, VAR_max_
+
+LABEL_5:
+		lw $a0, VAR_max_
+		li $v0, 1
+		syscall
+
+LABEL_END:
+		li $v0, 10
+		syscall
---
+++ launch_tester.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+make clean
+make
+
+GREEN='\033[0;32m'  
+NC='\033[0m'
+YELLOW='\033[1;33m'
+CYAN='\033[1;36m'
+
+
+echo "${YELLOW}Running compiler on all test files found in the /tests ${NC}";
+TOTAL=0;
+
+
+echo
+for f in tests/*.txt ; 
+do 
+    echo "${YELLOW}Running $f (${f%.txt}.asm)...${NC}";
+    ./scalpa -t -o "${f%.txt}.asm" < "$f" ;
+    TOTAL=$((TOTAL+1))
+done
+
+
+echo "\n${GREEN}*************************************************";
+echo "******** $TOTAL tests were done with success ********";
+echo "*************************************************";
+
